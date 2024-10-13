970ec42f71acdfa8e53c4e2c18305d99252b1f85
Shreyansh Anand <s-anand@ti.com>
2024-10-13 22:32:31 +0530

    Added am62d-r5 soc

--- soc/ti/k3/am6x/r5/soc.h
+++ soc/ti/k3/am6x/r5/soc.h
@@ -4,9 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef _TI_K3_J721E_R5_SOC_H_
-#define _TI_K3_J721E_R5_SOC_H_
+// #ifndef _TI_K3_J721E_R5_SOC_H_
+// #define _TI_K3_J721E_R5_SOC_H_
 
 #include <zephyr/drivers/interrupt_controller/intc_vim.h>
 
-#endif /* _TI_K3_J721E_R5_SOC_H_ */
+// #endif /* _TI_K3_J721E_R5_SOC_H_ */
